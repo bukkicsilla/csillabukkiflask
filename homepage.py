d44eaab93b9c219458d4275b7b42203e018b3dfa
csilla bukki <csillabukki@csillas-MacBook-Air-2.local>
2020-09-06 17:32:11 -0700

    homepage production

--- homepage.py
+++ homepage.py
@@ -8,32 +8,32 @@ def classical():
     return render_template('home.html')
     #return render_template('mfps.html')
 
-@app.route('/labor')  
+@app.route('/labor')
 def labor():
    return render_template('deeplook.html')
 
 
-@app.route('/launch')  
+@app.route('/launch')
 def launch():
    return render_template('androidapps.html')
 
 @app.route('/launch/amhelp')
 def amhelp():
    return render_template('amhelp.html')
-   
-#@app.route('/icebreaker')  
+
+#@app.route('/icebreaker')
 #def icebreaker():
 #   return render_template('icebreaker.html')
 
-#@app.route('/money')  
+#@app.route('/money')
 #def money():
 #   return render_template('math.html')
 
-#@app.route('/money/countpenny')  
+#@app.route('/money/countpenny')
 #def countpenny():
 #   return render_template('countpenny.html')
 
-@app.route('/interest')  
+@app.route('/interest')
 def interest():
    return render_template('interest.html')
 
@@ -155,17 +155,17 @@ def confusion():
    return render_template('confusion.html')
 
 
-@app.route('/study')  
+@app.route('/study')
 def study():
    return render_template('study.html')
 
 @app.route('/study/voronoi')
 def voronoi():
    return render_template('voronoi.html')
-   
+
 @app.route('/study/programming')
 def programming():
-   return render_template('programming.html')   
+   return render_template('programming.html')
 
 @app.route('/about')
 def about():
@@ -176,5 +176,4 @@ def about():
 
 
 if __name__ == '__main__':
-   app.run(debug=True)
-
+    app.run()
